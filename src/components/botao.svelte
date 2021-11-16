2919ab12384bbbd2dbde467e16489d39cd09eb4e
FHenric <ffilipehhenrique@gmail.com>
2021-11-15 23:27:14 -0300

    first commit

---
+++ src/components/botao.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let texto = ''
+    export let triplo = false
+    export let duplo = false
+    export let operacao = false
+    export let destaque = false
+    
+</script>
+
+<button class='botao' class:triplo class:duplo class:operacao class:destaque>
+    {texto}
+</button>
+
+<style>
+    .botao{
+        margin: 0;
+        border: 1px solid var(--cor-fundo);
+        flex-basis:25%;
+        cursor:pointer;
+    }
+    .duplo{
+        flex-basis:50%;
+    }
+    .triplo{
+        flex-basis:75%;
+    }
+    .operacao{
+        background-color:#3700F5;
+        color:black;
+    }
+    .destaque{
+        background-color:#B10BDE;
+        color:black;
+    }
+</style>
