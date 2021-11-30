9da90ebf77e238124bef5397d39ff6017ffda4bb
FHenric <ffilipehhenrique@gmail.com>
2021-11-30 15:32:49 -0300

    finalização do projeto / logica de programação

--- src/components/Calculadora.svelte
+++ src/components/Calculadora.svelte
@@ -4,52 +4,56 @@
     import Tela from './display.svelte';
     import CalculadoraModel from '../model/CalculadoraModel'
 
-    let calc = new CalculadoraModel;
+    let calc = new CalculadoraModel();
 
     const numeroDigitado = num => calc = calc.numeroDigitado(num);
     const pontoDigitado = () => calc = calc.pontoDigitado();
+    const operacaoDigitada = op => calc = calc.operacaoDigitada(op);
+    const calcular = () => calc = calc.calcular()
+    const limpar = () => calc = calc.limpar();
+
 </script>
 
 <div class="calculadora">
     <Tela valor={calc.valor}/>
     <Linha>
-        <Botao destaque triplo texto = "AC"/>
-        <Botao operacao texto = "/"/>
+        <Botao destaque triplo texto = "AC" onClick={limpar}/>
+        <Botao operacao texto = "/" onClick={operacaoDigitada}/>
     </Linha>
 
     <Linha>
         <Botao texto="1" onClick={numeroDigitado} />
         <Botao texto="2" onClick={numeroDigitado} />
         <Botao texto="3" onClick={numeroDigitado} />
-        <Botao operacao texto="*"/>
+        <Botao operacao texto="*" onClick={operacaoDigitada}/>
     </Linha>
 
     <Linha>
         <Botao texto="4" onClick={numeroDigitado} />
         <Botao texto="5" onClick={numeroDigitado} />
         <Botao texto="6" onClick={numeroDigitado} />
-        <Botao operacao texto="+"/>
+        <Botao operacao texto="+" onClick={operacaoDigitada}/>
     </Linha>
 
     <Linha>
         <Botao texto="7" onClick={numeroDigitado} />
         <Botao texto="8" onClick={numeroDigitado} />
         <Botao texto="9" onClick={numeroDigitado} />
-        <Botao operacao texto="-"/>
+        <Botao operacao texto="-" onClick={operacaoDigitada}/>
     </Linha>
 
     <Linha>
         <Botao duplo texto="0" onClick={numeroDigitado}/>
         <Botao texto="," onClick={pontoDigitado}/>
-        <Botao destaque texto="="/>
+        <Botao destaque texto="=" onClick={calcular} />
     </Linha>
 </div>
 
 <style>
     .calculadora{
         background-color: var(--cor-fundo);
-        height:220px;
-        width:150px;
+        height:300px;
+        width:250px;
         padding:5px;
         border-radius:5px;
 
--- src/model/CalculadoraModel.js
+++ src/model/CalculadoraModel.js
@@ -1,20 +1,53 @@
+//A lógica de programação melhor identada está no arquivo 'pre-compiler.ts'
+
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _CalculadoraModel_valor, _CalculadoraModel_acumulador, _CalculadoraModel_limparTela, _CalculadoraModel_operacao;
 const NAO_LIMPAR_TELA = false;
+const LIMPAR_TELA = true;
 export default class CalculadoraModel {
-    constructor(valor = '', acumulador = 0, operacao = '0', limparTela = false) {
-        this.valor = valor;
-        this.acumulador = acumulador;
-        this.limparTela = limparTela;
-        this.operacao = operacao;
+    constructor(valor = null, acumulador = null, operacao = null, limparTela = false) {
+        _CalculadoraModel_valor.set(this, void 0);
+        _CalculadoraModel_acumulador.set(this, void 0);
+        _CalculadoraModel_limparTela.set(this, void 0);
+        _CalculadoraModel_operacao.set(this, void 0);
+        __classPrivateFieldSet(this, _CalculadoraModel_valor, valor, "f");
+        __classPrivateFieldSet(this, _CalculadoraModel_acumulador, acumulador, "f");
+        __classPrivateFieldSet(this, _CalculadoraModel_limparTela, limparTela, "f");
+        __classPrivateFieldSet(this, _CalculadoraModel_operacao, operacao, "f");
     }
-    get Valor() {
+    get valor() {
         var _a;
-        return ((_a = this.valor) === null || _a === void 0 ? void 0 : _a.replace('.', ',')) || '0';
+        return ((_a = __classPrivateFieldGet(this, _CalculadoraModel_valor, "f")) === null || _a === void 0 ? void 0 : _a.replace('.', ',')) || '0';
     }
     numeroDigitado(novoValor) {
-        return new CalculadoraModel((this.limparTela || !this.valor) ? novoValor : this.valor + novoValor, this.acumulador, this.operacao, NAO_LIMPAR_TELA);
+        return new CalculadoraModel((__classPrivateFieldGet(this, _CalculadoraModel_limparTela, "f") || !__classPrivateFieldGet(this, _CalculadoraModel_valor, "f")) ? novoValor : __classPrivateFieldGet(this, _CalculadoraModel_valor, "f") + novoValor, __classPrivateFieldGet(this, _CalculadoraModel_acumulador, "f"), __classPrivateFieldGet(this, _CalculadoraModel_operacao, "f"), NAO_LIMPAR_TELA);
     }
     pontoDigitado() {
         var _a;
-        return new CalculadoraModel(((_a = this.valor) === null || _a === void 0 ? void 0 : _a.includes(',')) ? this.valor : this.valor + ',', this.acumulador, this.operacao, NAO_LIMPAR_TELA);
+        return new CalculadoraModel(((_a = __classPrivateFieldGet(this, _CalculadoraModel_valor, "f")) === null || _a === void 0 ? void 0 : _a.includes('.')) ? __classPrivateFieldGet(this, _CalculadoraModel_valor, "f") : __classPrivateFieldGet(this, _CalculadoraModel_valor, "f") + '.', __classPrivateFieldGet(this, _CalculadoraModel_acumulador, "f"), __classPrivateFieldGet(this, _CalculadoraModel_operacao, "f"), NAO_LIMPAR_TELA);
+    }
+    limpar() {
+        return new CalculadoraModel();
+    }
+    operacaoDigitada(proximaOperacao) {
+        return this.calcular(proximaOperacao);
+    }
+    calcular(proximaOperacao = null) {
+        const acumulador = !__classPrivateFieldGet(this, _CalculadoraModel_operacao, "f")
+            ? parseFloat(__classPrivateFieldGet(this, _CalculadoraModel_valor, "f"))
+            : eval(`${__classPrivateFieldGet(this, _CalculadoraModel_acumulador, "f")} ${__classPrivateFieldGet(this, _CalculadoraModel_operacao, "f")} ${__classPrivateFieldGet(this, _CalculadoraModel_valor, "f")}`);
+        const valor = !__classPrivateFieldGet(this, _CalculadoraModel_operacao, "f") ? __classPrivateFieldGet(this, _CalculadoraModel_valor, "f") : `${acumulador}`;
+        return new CalculadoraModel(valor, acumulador, proximaOperacao, proximaOperacao ? LIMPAR_TELA : NAO_LIMPAR_TELA);
     }
 }
+_CalculadoraModel_valor = new WeakMap(), _CalculadoraModel_acumulador = new WeakMap(), _CalculadoraModel_limparTela = new WeakMap(), _CalculadoraModel_operacao = new WeakMap();
